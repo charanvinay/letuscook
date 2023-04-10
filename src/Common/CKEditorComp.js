78d1d2b17bbcdb0b1a73572633e04c18def0945c
charanvinay_ael <charanvinay@auroraelabs.com>
2023-04-10 10:52:39 +0530

    changed ckedito with react quill

--- src/Common/CKEditorComp.js
+++ src/Common/CKEditorComp.js
@@ -1,25 +1,72 @@
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+const modules = {
+  toolbar: [
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+    ],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+/*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+const formats = [
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+];
 
 const CKEditorComp = (props) => {
+  const [editorHtml, setEditorHtml] = useState(props.value || null);
+  
+  const handleChange = (html) => {
+    console.log(html);
+    setEditorHtml(html);
+    props.handleChanges(html);
+  };
   return (
-    <CKEditor
-      editor={ClassicEditor}
-      config={{
-        placeholder: "Type your text here...",
-        // plugins: [ Paragraph, Bold, Italic, Essentials ],
-        toolbar: ["bold", "italic", "|", "bulletedList", "numberedList"],
-      }}
-      data={props.value}
-      onReady={(editor) => {
-        // editor.focus();
-      }}
-      key={props.id}
-      onChange={(event, editor) => {
-        const data = editor.getData();
-        props.handleChanges(data);
-      }}
+    // <CKEditor
+    //   editor={ClassicEditor}
+    //   config={{
+    //     placeholder: "Type your text here...",
+    //     // plugins: [ Paragraph, Bold, Italic, Essentials ],
+    //     toolbar: ["bold", "italic", "|", "bulletedList", "numberedList"],
+    //   }}
+    //   data={props.value}
+    //   onReady={(editor) => {
+    //     // editor.focus();
+    //   }}
+    //   key={props.id}
+    //   onChange={(event, editor) => {
+    //     const data = editor.getData();
+    //     props.handleChanges(data);
+    //   }}
+    // />
+    <ReactQuill
+      theme="snow"
+      onChange={handleChange}
+      value={editorHtml}
+      modules={modules}
+      formats={formats}
+      // bounds={".app"}
+      placeholder="Type your text here..."
     />
   );
 };
--- src/Common/Skeletons/CKeditor.js
+++ src/Common/Skeletons/CKeditor.js
@@ -4,8 +4,8 @@ import React from 'react'
 const CKeditor = () => {
   return (
     <Box sx={{padding: 0, margin: 0}}>
-      <Skeleton variant="rectangular" animation="wave" width={"100%"} height={40} sx={{marginBottom: "2px"}} />
-      <Skeleton variant="rectangular" animation="wave" width={"100%"} height={80} />
+      <Skeleton variant="rectangular" animation="wave" width={"100%"} height={40} sx={{marginBottom: "2px", borderRadius: "0.4em 0.4em 0 0"}} />
+      <Skeleton variant="rectangular" animation="wave" width={"100%"} height={80} sx={{borderRadius: "0 0 0.4em 0.4em"}}/>
     </Box>
   )
 }
